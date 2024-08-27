3f12a2b745a997d005d05a4259ffac65ff1d5ccc
vadyknyfy <vadyknyfy@gmail.com>
2024-08-27 16:08:27 +0300

    init

--- public/src/views/lr1/template.js
+++ public/src/views/lr1/template.js
@@ -1,12 +1,12 @@
-let selectedlaburlP ="/views/lr1/";
+let selectedlaburlP ="/src/views/lr1/";
 function initFunction(semestr){
     switch (semestr){
         case 1,"1":{
-            selectedlaburlP ="/views/lr1/";
-            $("#nav_menu1").html('<div class="select_item selected" laburl="/views/lr1/"> <p>Лабораторна номер 1</p></div>');
+            selectedlaburlP ="/src/views/lr1/";
+            $("#nav_menu1").html('<div class="select_item selected" laburl="/src/views/lr1/"> <p>Лабораторна номер 1</p></div>');
             let labinfodata
             $.ajax({
-                url: '/views/lr1/lab.json',
+                url: '/src/views/lr1/lab.json',
                 type: 'GET',
                 dataType: 'json',
                 success: function (data) {
@@ -32,8 +32,8 @@ function initFunction(semestr){
             break;
         }
         case 2,"2":{
-            selectedlaburlP ="/views/part2/mtiplr1/";
-            $("#nav_menu1").html('<div class="select_item selected" laburl="/views/part2/mtiplr1/"> <p>Лабораторна номер 1</p></div>');
+            selectedlaburlP ="/src/views/part2/mtiplr1/";
+            $("#nav_menu1").html('<div class="select_item selected" laburl="/src/views/part2/mtiplr1/"> <p>Лабораторна номер 1</p></div>');
             $.ajax({
                 url: selectedlaburlP+'info.json',
                 type: 'GET',
@@ -61,7 +61,7 @@ function initFunction(semestr){
             })
             let labinfodata
             $.ajax({
-                url: '/views/part2/lab.json',
+                url: '/src/views/part2/lab.json',
                 type: 'GET',
                 dataType: 'json',
                 success: function (data) {
