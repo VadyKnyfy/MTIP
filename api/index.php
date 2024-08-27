997fc4518d9cdd060acb4b943f582f0686646baa
vadyknyfy <vadyknyfy@gmail.com>
2024-08-27 15:40:45 +0300

    init

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php
-$redirect_url = $_SERVER['DOCUMENT_ROOT'] . '/api/src/views/lr1/template.html';
+$redirect_url = 'api/src/views/lr1/template.html';
 header('Location: ' . $redirect_url);
 exit;
